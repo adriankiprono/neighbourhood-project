6c4843ae34877df0670860418aad50beb8cf4f7a
adriankiprono <tuimuradrian6@gmail.com>
2020-01-17 12:35:28 +0300

    add class one

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name=models.CharField(max_length=30,null=True)
+    location=models.CharField(max_length=40,null=True)
+    occupants=models.PositiveIntegerField(null=True)
+
+
+
+
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adriantuimur/Desktop/moringa-python/neighbourhood/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
