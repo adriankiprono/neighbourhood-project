f436a5c5b91e3d190b6ad2c8fcfc9860b6152497
adriankiprono <tuimuradrian6@gmail.com>
2020-01-20 10:33:50 +0300

    add search.html

--- neighbour/views.py
+++ neighbour/views.py
@@ -11,3 +11,16 @@ def home(request):
     neighbourhood = Neighbourhood.objects.all()
     return render(request, 'home.html',locals())
 
+def search_results(request):
+    
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_business = Business.search_by_business_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-hood/search.html',{"message":message,"business": searched_business})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-hood/search.html',{"message":message})
+
